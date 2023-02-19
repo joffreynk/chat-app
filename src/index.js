46d0487f783b104719e46f8294687909d276b2f3
Joffreynk <joyjoffrey@gmail.com>
2023-02-19 10:51:47 +0200

    add browser router and add react18 main root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root  = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
