f495097db63e4ed3892e87e3cf7cf8727a0f0ec0
Joffreynk <joyjoffrey@gmail.com>
2023-02-19 11:12:20 +0200

    import chats and authprovider

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import React from "react"
 
 import { Routes, Route } from "react-router-dom"
 
-// import { AuthProvider } from "../contexts/AuthContext"
+import { AuthProvider } from "../contexts/AuthContext"
 
-// import Chats from "./Chats"
+import Chats from "./Chats"
 import Login from "./Login"
 
 function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
-      <Routes>
-        <Route path="/" element = {<Login />} />
-      </Routes>
+      <AuthProvider>
+          <Routes>
+            <Route path="/chats" component={Chats}/>
+            <Route path="/" element = {<Login />} />
+          </Routes>
+      </AuthProvider>
     </div>
   )
 }
