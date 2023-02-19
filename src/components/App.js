2a661af27b8e408af7d864aac1c479bf745a91c9
Joffreynk <joyjoffrey@gmail.com>
2023-02-19 10:50:43 +0200

    fix routing problems

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 // import { AuthProvider } from "../contexts/AuthContext"
 
@@ -10,14 +10,9 @@ import Login from "./Login"
 function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
-      <Router>
-        {/* <AuthProvider> */}
-          <Routes>
-            {/* <Route path="/chats" component={Chats} /> */}
-            <Route path="/" component={Login} />
-          </Routes>
-        {/* </AuthProvider> */}
-      </Router>
+      <Routes>
+        <Route path="/" element = {<Login />} />
+      </Routes>
     </div>
   )
 }
