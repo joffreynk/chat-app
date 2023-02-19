303b7d949d7be6e72b8033a173bef8d1ca0df527
Joffreynk <joyjoffrey@gmail.com>
2023-02-19 11:08:19 +0200

    create user context and export it

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,27 @@
-import React, {useEffect, useState, useContext} from "react";
+import {useEffect, useState, useContext, createContext} from "react";
 import {useHistory } from 'react-router-dom'
 import { auth } from "../firebase";
+
+const AuthContext = createContext()
+export const useAuth = ()=>useContext(AuthContext)
+
+export const AuthProvider = ({ children }) =>{
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState({})
+  const history = useHistory();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged(user=>{
+      setLoading(false)
+      setUser(user)
+      history('/chats')
+    }) //
+  }, [user, history])
+
+  return (
+    <AuthContext.Provider value={user} >
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+
+}
