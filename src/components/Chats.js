459c3b39132769a165dcce14ae3b3d0827edc947
Joffreynk <joyjoffrey@gmail.com>
2023-02-20 20:09:41 +0200

    implement chat headers

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -5,3 +5,24 @@ import { useAuth } from '../contexts/AuthContext'
 
 import { auth } from '../firebase'
 import { userAuth } from '../contexts/AuthContext'
+
+const Chats = () => {
+
+  return (
+    <div className='chats-page'>
+      <div className='nav-bar'>
+        <div className='logo-tab'>
+          CR
+        </div>
+        <div 
+        className='logout-tab'
+        onClick={handleLogout}
+        >Log out</div>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Chats
