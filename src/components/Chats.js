2be8800cbc9b4e9897fc672fb966c88753186f7a
Joffreynk <joyjoffrey@gmail.com>
2023-02-20 20:08:10 +0200

    import functionalities to use

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -2,3 +2,6 @@ import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
+
+import { auth } from '../firebase'
+import { userAuth } from '../contexts/AuthContext'
