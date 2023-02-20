dbd91552aa7be872037110ca05d0028b92bba3b9
Joffreynk <joyjoffrey@gmail.com>
2023-02-20 20:06:06 +0200

    rename user auth context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { auth } from "../firebase";
 
 const AuthContext = React.createContext();
-export const useAuth = () => useContext(AuthContext);
+export const userAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) =>{
   const [loading, setLoading] = useState(true)
