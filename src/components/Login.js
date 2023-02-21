059722db1e3375d493f8e6d83e30f5fe71965fda
Joffreynk <joyjoffrey@gmail.com>
2023-02-21 19:51:39 +0200

    update Login data

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons'
 
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/compat/app";
 import { auth } from '../firebase';
 const Login = () => {
   return (
